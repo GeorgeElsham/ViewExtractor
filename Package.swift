c233227f9e1fc11f2d7c34109e2c54c8a34235c8
George <40073010+GeorgeElsham@users.noreply.github.com>
2021-07-26 21:25:23 +0100

    Feature - get view by index (#6)
    * Ran SwiftFormat for consistent formatting.
    
    * Added comments on how the code works.
    
    * Improved checking for `EmptyView` and `ForEach` content.
    
    * Implemented way to get a specific view, without loading everything.

--- Package.swift
+++ Package.swift
@@ -6,13 +6,14 @@ import PackageDescription
 let package = Package(
     name: "ViewExtractor",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ViewExtractor",
-            targets: ["ViewExtractor"]),
+            targets: ["ViewExtractor"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,9 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ViewExtractor",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "ViewExtractorTests",
-            dependencies: ["ViewExtractor"]),
+            dependencies: ["ViewExtractor"]
+        ),
     ]
 )
